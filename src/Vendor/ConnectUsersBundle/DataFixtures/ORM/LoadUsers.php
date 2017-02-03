8972e5c1bc99ecb6e5cf19c9f0813241384adc6b
sandy <sdelhoute@gmail.com>
2017-02-03 01:51:37 +0100

    modification du menu admin, modification des formulaire de connexion , ajout route reset password

--- src/Vendor/ConnectUsersBundle/DataFixtures/ORM/LoadUsers.php
+++ src/Vendor/ConnectUsersBundle/DataFixtures/ORM/LoadUsers.php
@@ -8,27 +8,23 @@ use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Vendor\ConnectUsersBundle\Entity\usersweb;
 use Vendor\ConnectUsersBundle\Entity\usersemployee;
-use Symfony\Component\Validator\Constraints\DateTime;
+use Vendor\ConnectUsersBundle\Entity\status;
+use \DateTime;
+
+
 class Loadusers implements FixtureInterface
 {
 
 public Function load(ObjectManager $manager){
 
+
+$status=new status();
+$status->setName("Super_Admin");
 $users = new Usersweb();
 $users->setName('sandy');
 $users->setFirstname('delhoute');
 $users->setEmail('sdelhoute@gmail.com');
 $users->setPassword(hash('sha1','123456'));
-$users1 = new Usersweb();
-$users1->setName('guillaume');
-$users1->setFirstname('lemaire');
-$users1->setEmail('wl.guillaume@gmail.com');
-$users1->setPassword(hash('sha1','123456'));
-$users2 = new usersweb();
-$users2->setName('fabien');
-$users2->setFirstname('coo');
-$users2->setEmail('fabien.coo@gmail.com');
-$users2->setPassword(hash('sha1','123456'));
 $usersemployee = new usersemployee();
 $usersemployee->setName('fabien');
 $usersemployee->setFirstname('coo');
@@ -36,14 +32,12 @@ $usersemployee->setEmail('fabien.coo@mealandbox.fr');
 $usersemployee->setPassword(hash('sha1','0123456'));
 $usersemployee->setBirthdate(new DateTime('02/31/2011'));
 $usersemployee->setNumbersocial('188115912345678');
+$usersemployee->setStatus($status->getId());
 
 
 
-
+$manager->persist($status);
 $manager->persist($users);
-$manager->persist($users1);
-$manager->persist($users2);
-
 $manager->persist($usersemployee);
 
 $manager->flush();
