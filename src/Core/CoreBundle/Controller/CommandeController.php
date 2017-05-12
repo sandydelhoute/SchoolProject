59d2d1cc1b670c18fd53c7467eb5771fd982bf7a
sandy <s.delhoute@sfr.fr>
2017-05-12 10:26:51 +0200

    configuration du panier ajouter

--- src/Core/CoreBundle/Controller/CommandeController.php
+++ src/Core/CoreBundle/Controller/CommandeController.php
@@ -9,11 +9,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Core\CoreBundle\Entity\PayCards;
 use Core\CoreBundle\Form\PayCardsType;
 use Core\CoreBundle\Entity\OrderClient;
+use Core\CoreBundle\Entity\OrderLine;
 use Symfony\Component\Validator\Constraints\DateTime;
 class CommandeController extends Controller
 {
      public function commandeAction(Request $request)
     {
+        $valideCommande=false;
+        $total=0;
+        $ptsFideleCommande=0;
     	$session = $request->getSession();
     	$em = $this->getDoctrine()->getManager();
 		$listOrderLine=$session->get('panier');
@@ -27,28 +31,34 @@ class CommandeController extends Controller
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-
             $currentuser= $this->getUser();
             $orderclient = new OrderClient();
             $orderclient->setUsers($currentuser);
             $orderclient ->setDatePurchase(new \DateTime('NOW'));
-            foreach ($listOrderLine as $orderline) {
+            foreach ($listOrderLine as $key => $orderline  ) {
                 $orderclient->addOrderLine($orderline);
                 $orderline->setOrderClient($orderclient);
+                $total += $total + $orderline->getQuantity() * $orderline->getProduct()->getPrix();
             }
-
+        $ptsFideleCommande=round($total/10, 2);
+        $valideCommande=true;
+        $listOrderLine=array();
+        $currentuser->setRewardPoints($currentuser->getRewardPoints()+$ptsFideleCommande);
         $currentuser->addOrder($orderclient);
         $em->persist($currentuser);
         $em->flush();
+
         }
-        return $this->render('CoreCoreBundle:Commande:commandelayout.html.twig',array('listOrderLine'=>$listOrderLine,'form'=>$form->createView()));
+        return $this->render('CoreCoreBundle:Commande:commandelayout.html.twig',array('listOrderLine'=>$listOrderLine,'form'=>$form->createView(),'valideCommande'=>$valideCommande ,'totalCommande'=>$total,'ptsFideleCommande'=>$ptsFideleCommande));
     }
 
-     public function downloadResumeOrderAction($id)
+    public function downloadResumeOrderAction($id)
     {
-        $html = $this->renderView(':pdf:invoice.html.twig');
-
-        $filename = sprintf('test-%s.pdf', date('Y-m-d'));
+        $em = $this->getDoctrine()->getManager();
+        $order= $em->getRepository('CoreCoreBundle:OrderClient')->findOneById($id);
+        $html = $this->renderView(':pdf:invoice.html.twig',array('order'=>$order));
+        $date=$order->getDatePurchase();
+        $filename = sprintf('Commande-%s.pdf',$date->format('Y-m-d'));
 
         return new Response(
             $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
